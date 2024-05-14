b5e2eff2cc43b70b0fdc190647ed0cb1efe99b2f
fedotovps <fedotovps@bk.ru>
2024-05-14 14:16:17 +0300

    Добавление разметки HTML

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="ru">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="css/style.css" />
+          <title>Skypro</title>
+        </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
