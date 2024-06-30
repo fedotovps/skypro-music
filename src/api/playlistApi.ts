d048e9c960479a33a46aa1d421c08c79a18aa81d
fedotovps <fedotovps@bk.ru>
2024-06-30 15:37:38 +0300

    Добавил кеширование в запрос плейлистов

--- src/api/playlistApi.ts
+++ src/api/playlistApi.ts
@@ -1,7 +1,12 @@
 const urlPlaylist = "https://skypro-music-api.skyeng.tech/catalog/selection/";
 
 export const playlistApi = async (id: string) => {
-    const response = await fetch(urlPlaylist + id);
+    const response = await fetch(urlPlaylist + id, 
+        {
+            method: "GET",
+            cache: "no-cache",
+        }
+    );
     if (!response.ok) {
         throw new Error("Ошибка в получении плейслистов");
     }
