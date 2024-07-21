ca3990738a728ae2a1081706e628b7238f8f02c7
fedotovps <fedotovps@bk.ru>
2024-07-21 18:46:19 +0300

    Редирект со страницы избранных, если нет авторизации

--- src/app/tracks/favourites/page.tsx
+++ src/app/tracks/favourites/page.tsx
@@ -6,10 +6,26 @@ import { Sidebar } from "@/components/Sidebar/Sidebar";
 import { Search } from "@/components/Search/Search";
 import { Track } from "@/components/Main/Main.types";
 import { useAppSelector } from "@/store/store";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const FavouritesTracksPage = () => {
   const tracks: Track[] = useAppSelector((state) => state.player.likedTracks);
   let errorMessage: string | null = null;
+
+  const router = useRouter();
+  const logged = useAppSelector((state) => state.auth.tokens.access);
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    setIsInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (isInitialized && !logged) {
+      router.push("/tracks");
+    }
+  }, [logged, isInitialized, router]);
 
   return (
     <>
